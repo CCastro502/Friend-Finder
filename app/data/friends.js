c5d5395bb594fa65c8a9b0a047a465bf34c55dd5
CCastro <chris502castro@gmail.com>
2018-12-19 15:49:30 -0700

    Added a css page to style survey and home page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,11 @@
 var friendsArray = [
-    {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
-    }
-  ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
-  
+  {
+    customerName: "Ahmed",
+    customerEmail: "ahmed@example.com",
+    customerID: "afhaque89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
